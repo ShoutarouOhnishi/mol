ad457d6a48014c12fe9a57be29e8e1159f38630a
ShoutarouOhnishi <boderline3@gmail.com>
2023-09-26 14:49:46 +0900

    frontend: OpenApiコード自動生成

---
+++ frontend/openapi/client/lib/api/account_api.dart
@@ -0,0 +1,114 @@
+//
+// AUTO-GENERATED FILE, DO NOT MODIFY!
+//
+// @dart=2.12
+
+// ignore_for_file: unused_element, unused_import
+// ignore_for_file: always_put_required_named_parameters_first
+// ignore_for_file: constant_identifier_names
+// ignore_for_file: lines_longer_than_80_chars
+
+part of openapi.api;
+
+
+class AccountApi {
+  AccountApi([ApiClient? apiClient]) : apiClient = apiClient ?? defaultApiClient;
+
+  final ApiClient apiClient;
+
+  /// ユーザー作成
+  ///
+  /// Note: This method returns the HTTP [Response].
+  ///
+  /// Parameters:
+  ///
+  /// * [CreateUserRequest] createUserRequest (required):
+  Future<Response> createUserWithHttpInfo(CreateUserRequest createUserRequest,) async {
+    // ignore: prefer_const_declarations
+    final path = r'/api/v1/user';
+
+    // ignore: prefer_final_locals
+    Object? postBody = createUserRequest;
+
+    final queryParams = <QueryParam>[];
+    final headerParams = <String, String>{};
+    final formParams = <String, String>{};
+
+    const contentTypes = <String>['application/json'];
+
+
+    return apiClient.invokeAPI(
+      path,
+      'POST',
+      queryParams,
+      postBody,
+      headerParams,
+      formParams,
+      contentTypes.isEmpty ? null : contentTypes.first,
+    );
+  }
+
+  /// ユーザー作成
+  ///
+  /// Parameters:
+  ///
+  /// * [CreateUserRequest] createUserRequest (required):
+  Future<CreateUserResponse?> createUser(CreateUserRequest createUserRequest,) async {
+    final response = await createUserWithHttpInfo(createUserRequest,);
+    if (response.statusCode >= HttpStatus.badRequest) {
+      throw ApiException(response.statusCode, await _decodeBodyBytes(response));
+    }
+    // When a remote server returns no body with a status of 204, we shall not decode it.
+    // At the time of writing this, `dart:convert` will throw an "Unexpected end of input"
+    // FormatException when trying to decode an empty string.
+    if (response.body.isNotEmpty && response.statusCode != HttpStatus.noContent) {
+      return await apiClient.deserializeAsync(await _decodeBodyBytes(response), 'CreateUserResponse',) as CreateUserResponse;
+    
+    }
+    return null;
+  }
+
+  /// ログイン
+  ///
+  /// Note: This method returns the HTTP [Response].
+  Future<Response> loginWithHttpInfo() async {
+    // ignore: prefer_const_declarations
+    final path = r'/api/v1/authenticate';
+
+    // ignore: prefer_final_locals
+    Object? postBody;
+
+    final queryParams = <QueryParam>[];
+    final headerParams = <String, String>{};
+    final formParams = <String, String>{};
+
+    const contentTypes = <String>[];
+
+
+    return apiClient.invokeAPI(
+      path,
+      'POST',
+      queryParams,
+      postBody,
+      headerParams,
+      formParams,
+      contentTypes.isEmpty ? null : contentTypes.first,
+    );
+  }
+
+  /// ログイン
+  Future<LoginResponse?> login() async {
+    final response = await loginWithHttpInfo();
+    if (response.statusCode >= HttpStatus.badRequest) {
+      throw ApiException(response.statusCode, await _decodeBodyBytes(response));
+    }
+    // When a remote server returns no body with a status of 204, we shall not decode it.
+    // At the time of writing this, `dart:convert` will throw an "Unexpected end of input"
+    // FormatException when trying to decode an empty string.
+    if (response.body.isNotEmpty && response.statusCode != HttpStatus.noContent) {
+      return await apiClient.deserializeAsync(await _decodeBodyBytes(response), 'LoginResponse',) as LoginResponse;
+    
+    }
+    return null;
+  }
+}
