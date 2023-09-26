4a44ed1e045c7d295b55ec702a8cf8b79bf6bf64
ShoutarouOhnishi <boderline3@gmail.com>
2023-09-26 14:51:29 +0900

    frontend: 認証周り テストコード実装

---
+++ frontend/test/main_test.dart
@@ -0,0 +1,32 @@
+import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:firebase_core_platform_interface/firebase_core_platform_interface.dart';
+import 'package:frontend/main.dart';
+import 'package:frontend/view_models/splash_view_model.dart';
+import 'package:mockito/annotations.dart';
+import 'main_test.mocks.dart';
+
+@GenerateMocks([SplashViewStateNotifier])
+void main() {
+  setUp(() async {
+    // Firebaseのモッキング
+    setupFirebaseCoreMocks();
+    // Firebaseモッキング後に初期化
+    await Firebase.initializeApp();
+  });
+
+  testWidgets('スプラッシュスクリーンが正常に表示されている', (WidgetTester tester) async {
+    await tester.pumpWidget(
+      ProviderScope(
+        overrides: [
+          splashViewStateProvider.overrideWith((ref) =>
+              MockSplashViewStateNotifier()) // initializeメソッドでの状態変化による画面遷移を防ぐため、モックを使用
+        ],
+        child: const MyApp(),
+      ),
+    );
+    await tester.pumpAndSettle();
+    expect(find.text('Splash'), findsOneWidget);
+  });
+}
---
+++ frontend/test/shared_notifiers/auth_state_notifier_test.dart
@@ -0,0 +1,103 @@
+import 'package:flutter_test/flutter_test.dart';
+import 'package:mockito/annotations.dart';
+import 'package:mockito/mockito.dart';
+import 'package:frontend/shared_notifiers/auth_state_notifier.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:frontend/repositories/account_repository.dart';
+import 'package:frontend/services/firebase_auth_service.dart';
+import 'package:openapi/api.dart';
+
+import 'auth_state_notifier_test.mocks.dart';
+
+@GenerateMocks([FirebaseAuthService, AccountRepository, User])
+void main() {
+  group('initialize', () {
+    late MockFirebaseAuthService mockFirebaseAuthService;
+    late MockAccountRepository mockAccountRepository;
+    late AuthStateNotifier authStateNotifier;
+
+    setUp(() {
+      mockFirebaseAuthService = MockFirebaseAuthService();
+      mockAccountRepository = MockAccountRepository();
+      authStateNotifier =
+          AuthStateNotifier(mockFirebaseAuthService, mockAccountRepository);
+    });
+
+    test('FirebaseUserが取得できない場合にunauthenticatedとなる', () async {
+      when(mockFirebaseAuthService.currentUser).thenReturn(null);
+
+      await authStateNotifier.initialize();
+
+      authStateNotifier.currentState.when(
+        initial: () => fail('Should not be initial state'),
+        authenticated: (token) => fail('Should not be authenticated state'),
+        unauthenticated: () => expect(true, true), // Expected state
+        error: (message) => fail('Should not be error state'),
+      );
+    });
+
+    test('idTokenが取得できない場合にunauthenticatedとなる', () async {
+      final mockUser = MockUser();
+      when(mockFirebaseAuthService.currentUser).thenReturn(mockUser);
+      when(mockUser.getIdToken()).thenAnswer((_) => Future.value(null));
+
+      await authStateNotifier.initialize();
+
+      authStateNotifier.currentState.when(
+        initial: () => fail('Should not be initial state'),
+        authenticated: (token) => fail('Should not be authenticated state'),
+        unauthenticated: () => expect(true, true), // Expected state
+        error: (message) => fail('Should not be error state'),
+      );
+    });
+
+    test('バックエンド側でログインできなかった場合にunauthenticatedとなる', () async {
+      final mockUser = MockUser();
+      when(mockFirebaseAuthService.currentUser).thenReturn(mockUser);
+      when(mockUser.getIdToken()).thenAnswer((_) => Future.value('idToken'));
+      when(mockAccountRepository.login('idToken'))
+          .thenAnswer((_) async => null);
+
+      await authStateNotifier.initialize();
+
+      authStateNotifier.currentState.when(
+        initial: () => fail('Should not be initial state'),
+        authenticated: (token) => fail('Should not be authenticated state'),
+        unauthenticated: () => expect(true, true), // Expected state
+        error: (message) => fail('Should not be error state'),
+      );
+    });
+
+    test('ログイン成功時にauthenticatedとなる', () async {
+      final mockUser = MockUser();
+      when(mockFirebaseAuthService.currentUser).thenReturn(mockUser);
+      when(mockUser.getIdToken())
+          .thenAnswer((_) async => Future.value('idToken'));
+      when(mockAccountRepository.login('idToken'))
+          .thenAnswer((_) async => LoginResponse(token: 'token', userId: 1));
+
+      await authStateNotifier.initialize();
+
+      authStateNotifier.currentState.when(
+        initial: () => fail('Should not be initial state'),
+        authenticated: (token) => expect(true, true),
+        unauthenticated: () => fail('Should not be unauthenticated state'),
+        error: (message) => fail('Should not be error state'),
+      );
+    });
+
+    test('Exception発生時にerrorとなる', () async {
+      when(mockFirebaseAuthService.currentUser)
+          .thenThrow(Exception('Error occurred'));
+
+      await authStateNotifier.initialize();
+
+      authStateNotifier.currentState.when(
+        initial: () => fail('Should not be initial state'),
+        authenticated: (token) => fail('Should not be authenticated state'),
+        unauthenticated: () => fail('Should not be unauthenticated state'),
+        error: (message) => expect(true, true),
+      );
+    });
+  });
+}
